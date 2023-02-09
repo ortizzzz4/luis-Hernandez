f4bd344ff2c6dd8647618ba3ddcd932e3a7d2ecd
luis ortiz <107637800+ortizzzz4@users.noreply.github.com>
2023-02-09 11:41:37 -0600

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Odoo Academy",
+    'summary': """Academy app to manage Trailing""",
+    'description': "\n"
+                    "    Academy Module to manage Training \n"
+                    "    -Courses\n"
+                    "    -Sessions\n"
+                    "    -Attendees\n"
+                    "    ",
+    "author":"odoo",
+    'website': "https://www.odoo.com",
+    "category": 'Training',
+    'version': "0.1",
+    'depends':["base"],
+    "data":[
+
+    ],
+    "demo":[
+
+    ],
+
+
+}
+
