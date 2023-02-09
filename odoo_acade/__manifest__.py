7d3e4829177d2193445f173c796fa98d6153f610
ortizzzz4 <luisenriquehernandezortiz81@gmail.com>
2023-02-09 16:21:35 +0000

    [ADD] Basic Module

---
+++ odoo_acade/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+(
+    
+    "name":"odoo academy",
+    "summary":"Academy app to",
+    "description":"""Academy to module training:
+    -Courses
+    -Sessions
+    -Attenders
+    """,
+    "author":"odoo",
+    "website":"https://wwww.odoo.com",
+    "version":"0.1",
+    "depend":["base"],
+    "data":[
+    ],
+    "odoo":[
+    ],
+)
