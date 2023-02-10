637d7c4c20ed166e95b0cbc746992b9aba73e3ae
luis ortiz <107637800+ortizzzz4@users.noreply.github.com>
2023-02-10 13:05:05 -0600

    "cour"

---
+++ odoo_acade/models/course.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = "academy.course"
+    _description = "Course Info "
+    
+    name = fields.Char(string = 'Title', required=True)
+    description = fields.Text(string= 'Description')
+    
+    level = fields.Selection(string='Level',
+                            selection =[('beginner', 'Beginner'),
+                                        ('intermediate','Intermediate'),
+                                       ('advance','Advance')],   
+                            copy=False)
+    active = fields.Boolean(string='Active', default=True)
+    
+    country = fields.Selectio=(string='Level',selection =[('El Salvador', 'San Salvador'),
+                                        ('Guatemala','Guatemala'),
+                                       ('Mexico','Mexico DF')],   
